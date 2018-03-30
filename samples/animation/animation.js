da86023dff2ddc7bf97b33aade76de14728a9222
Quincy <466217509@qq.com>
2018-03-30 08:01:43 +0800

    闪烁动画

--- samples/animation/animation.js
+++ samples/animation/animation.js
@@ -1,3 +1,56 @@
-function init(){
+function init() {
+  dataModel = new ht.DataModel();
+  graphView = new ht.graph.GraphView(dataModel);
+  view = graphView.getView();
 
+  view.className = 'main';
+  document.body.appendChild(view);
+  dataModel.enableAnimation();
+
+  showOrHideNode(initNode(dataModel, 0))
+  moveNode(initNode(dataModel, 50))
+
+}
+
+function initNode(dataModel, n) {
+  var node = new ht.Node();
+  node.setName('Rect');
+  node.setSize(60, 60);
+  node.setPosition(150 + n, 50);
+  node.setStyle('shape', 'rect');
+  dataModel.add(node);
+  return node;
+}
+
+function showOrHideNode(node) {
+  node.setAnimation({
+    show: {
+      property: "opacity",
+      accessType: "style",
+      from: 1,
+      to: 0,
+      repeat: true
+    },
+    start: ["show"]
+  });
+}
+
+function moveNode(node) {
+  node.setAnimation({
+    moveToLeft: {
+      property: "position",
+      accessType: "style",
+      from: 300,
+      to: 150,
+      next: "moveToRight"
+    },
+    moveToRight: {
+      property: "position",
+      accessType: "style",
+      from: 150,
+      to: 300,
+      next: "moveToLeft"
+    },
+    start: ["moveToRight"]
+  });
 }
